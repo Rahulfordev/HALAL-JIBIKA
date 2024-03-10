618fdf9faf6d336b1d27c7fceeaafe8d1160461e
rahulfordev <rahulforsms@gmail.com>
2024-03-10 19:39:01 +0600

    error fix

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAhZus3mf8k-mBIfvuYM5hpUcgcVamRAyg",
-  authDomain: "react-app-399b7.firebaseapp.com",
-  projectId: "react-app-399b7",
-  storageBucket: "react-app-399b7.appspot.com",
-  messagingSenderId: "938464876614",
-  appId: "1:938464876614:web:1be1abf50a4f0932001247",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MSG_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
